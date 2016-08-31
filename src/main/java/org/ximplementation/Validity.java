0991256519e6d9e0bc79adacb1cab0f1898c092d
earthangry@gmail.com <earthangry@gmail.com>
2016-08-31 10:50:13 +0800

    Change Validity to English

--- src/main/java/org/ximplementation/Validity.java
+++ src/main/java/org/ximplementation/Validity.java
@@ -20,13 +20,19 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
- * 有效性注解。
+ * Validity annotation.
  * <p>
- * <i>有效性注解</i>标注于<i>实现者</i>类内的<i>实现方法</i>，用于设置<i>实现方法</i>的<i>有效性方法</i>。
+ * This annotation indicate that the implement method has an invocation
+ * validation by a validity method, the validity method should be invoked first
+ * for determining if the implement method is applicable for the current
+ * <i>implementee</i> method invocation.
+ * </p>
+ * <p>
+ * This annotation should be annotated on implement method.
  * </p>
  * 
  * @author earthangry@gmail.com
- * @date 2015年12月3日
+ * @date 2015-12-3
  *
  */
 @Retention(RetentionPolicy.RUNTIME)
@@ -34,14 +40,17 @@ import java.lang.annotation.Target;
 public @interface Validity
 {
 	/**
-	 * 有效性方法标识。
-	 * 
+	 * The identification of the validity method.
 	 * <p>
-	 * 如果注解类内存在被{@linkplain Refered}注解、且值为此标识的方法，那么它将被用作有效性方法；
+	 * It can be any of the following values :
 	 * </p>
-	 * <p>
-	 * 否则，方法名称为此标识、且返回值为{@code boolean}或者{@linkplain Boolean}的方法将被用作有效性方法。
-	 * </p>
+	 * <ul>
+	 * <li>The name of the validity method if no duplication;</li>
+	 * <li>The {@linkplain Refered#value() @Refered.value} of the validity
+	 * method;</li>
+	 * <li>The {@code java.lang.reflect.Method.toString()} value of the validity
+	 * method.</li>
+	 * </ul>
 	 * 
 	 * @return
 	 */
