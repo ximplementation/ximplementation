d5c42c9af38b7929c44d30fa20b3c9be098e2238
earthangry@gmail.com <earthangry@gmail.com>
2016-08-31 10:12:34 +0800

    Change Priority doc to English

--- src/main/java/org/ximplementation/Priority.java
+++ src/main/java/org/ximplementation/Priority.java
@@ -20,13 +20,17 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
- * 优先级注解。
+ * Priority annotation.
  * <p>
- * <i>优先级注解</i>标注于<i>实现者</i>类内的<i>实现方法</i>，用于设置<i>实现方法</i>的<i>优先级方法</i>。
+ * This annotation indicate that the implement method has an invocation
+ * priority, either a fixed value or the result of a priority method invocation.
+ * </p>
+ * <p>
+ * This annotation should be annotated on implement method.
  * </p>
  * 
  * @author earthangry@gmail.com
- * @date 2015年12月3日
+ * @date 2015-12-3
  *
  */
 @Retention(RetentionPolicy.RUNTIME)
@@ -34,9 +38,9 @@ import java.lang.annotation.Target;
 public @interface Priority
 {
 	/**
-	 * 优先级值。
+	 * The fixed priority value.
 	 * <p>
-	 * 此属性的默认值为{@code 0}。
+	 * Its default value is {@code 0}。
 	 * </p>
 	 * 
 	 * @return
@@ -44,17 +48,17 @@ public @interface Priority
 	int value() default 0;
 
 	/**
-	 * 优先级方法标识。
-	 * 
+	 * The identification of the priority method.
 	 * <p>
-	 * 如果注解类内存在被{@linkplain Refered}注解、且值为此标识的方法，那么它将被用作优先级方法；
+	 * It can be any the following values :
 	 * </p>
-	 * <p>
-	 * 否则，方法名称为此标识、且返回值为{@code int}或者{@linkplain Integer}的方法将被用作优先级方法。
-	 * </p>
-	 * <p>
-	 * 此属性的默认值为{@code 0}。
-	 * </p>
+	 * <ul>
+	 * <li>The name of the priority method if no duplication;</li>
+	 * <li>The {@linkplain Refered#value() @Refered.value} of the priority
+	 * method;</li>
+	 * <li>The {@code java.lang.reflect.Method.toString()} value of the priority
+	 * method.</li>
+	 * </ul>
 	 * 
 	 * @return
 	 */
